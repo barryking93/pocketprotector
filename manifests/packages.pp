56b7251a1dc8b48d60ebf7cc7074b3fc2ae40479
barry king <barryking93@gmail.com>
2023-05-04 15:23:46 -0700

    turning package lookup into case statement

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,11 +1,17 @@
 
 class pocketprotector::packages {
-  if lookup("pocketprotector::packages", undef, 'deep', false) != false: {
-    lookup("pocketprotector::packages", undef, 'deep', undef):
-      ensure => installed;
+  case lookup("pocketprotector::packages", undef, 'deep', false) {
+    false: {}
+    default: {
+      lookup("pocketprotector::packages", undef, 'deep', undef):
+        ensure => installed;
+    }
   }
-  if lookup("pocketprotector::packages::latest", undef, 'deep', false) != true: {
-    lookup("pocketprotector::packages::latest", undef, 'deep', undef):
-      ensure => latest;
+  case lookup("pocketprotector::packages.latest", undef, 'deep', false) {
+    false: {}
+    default: {
+      lookup("pocketprotector::packages::latest", undef, 'deep', undef):
+        ensure => latest;
+    }
   }
 }
