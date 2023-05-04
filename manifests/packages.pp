24142822ea72e93de4fc3aaf065c48225248740c
barry king <barryking93@gmail.com>
2023-05-04 15:08:55 -0700

    if, only if

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,10 +1,10 @@
 
 class pocketprotector::packages {
-  if lookup("pocketprotector::packages", undef, 'deep', false): {
+  if lookup("pocketprotector::packages", undef, 'deep', false) == true: {
     lookup("pocketprotector::packages", undef, 'deep', undef):
       ensure => installed;
   }
-  if lookup("pocketprotector::packages::latest", undef, 'deep', false): {
+  if lookup("pocketprotector::packages::latest", undef, 'deep', false) == true: {
     lookup("pocketprotector::packages::latest", undef, 'deep', undef):
       ensure => latest;
   }
