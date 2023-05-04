ab4c51013560523dff05d37f1f87b4713e9b6918
barry king <barryking93@gmail.com>
2023-05-04 15:55:49 -0700

    declare packages

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -4,15 +4,19 @@ class pocketprotector::packages {
   case lookup('pocketprotector::packages', undef, 'deep', false) {
     false: {}
     default: {
-      lookup('pocketprotector::packages', undef, 'deep', undef):
-        ensure => installed;
+      package {
+        lookup('pocketprotector::packages', undef, 'deep', undef):
+          ensure => installed;
+      }
     }
   }
   case lookup('pocketprotector::packages::latest', undef, 'deep', false) {
     false: {}
     default: {
-      lookup('pocketprotector::packages::latest', undef, 'deep', undef):
-        ensure => latest;
+      package {
+        lookup('pocketprotector::packages::latest', undef, 'deep', undef):
+          ensure => latest;
+      }
     }
   }
 }
