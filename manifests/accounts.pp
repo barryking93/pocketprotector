48b631292ca94633655acdf77423050441cb31b8
barry king <barryking93@gmail.com>
2023-05-05 10:51:32 -0700

    one hurdle cleared

--- manifests/accounts.pp
+++ manifests/accounts.pp
@@ -5,15 +5,15 @@
 # crawl pocketprotector.accounts in all
 
 class pocketprotector::accounts {
-  lookup('pocketprotector.accounts', undef, 'unique', undef).each |String $username, Hash $userhash| {
+  lookup('pocketprotector.accounts', undef, 'deep', undef).each |String $username, Hash $userhash| {
     #notify {"debug account for ${username}":}
     accounts::user { "${username}":
       home     => lookup("pocketprotector::accounts.${username}.home", undef, 'first', "/home/${username}"),
       uid      => lookup("pocketprotector::accounts.${username}.uid", undef, 'first', undef),
-      groups   => lookup("pocketprotector::accounts.${username}.groups", undef, 'hash', ['users']),
+      groups   => lookup("pocketprotector::accounts.${username}.groups", undef, 'deep', ['users']),
       password => lookup("pocketprotector::passwords.${username}", undef, 'first', '!!'),
       shell    => lookup("pocketprotector::accounts.${username}.shell", undef, 'first', '/bin/bash'),
-      sshkeys  => lookup("pocketprotector::accounts.${username}.sshkeys", Array[String], 'hash', []),
+      sshkeys  => lookup("pocketprotector::accounts.${username}.sshkeys", Array[String], 'deep', []),
     }
   }
 }
