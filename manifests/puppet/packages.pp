bf0bf6db42b17dd5516d6aff9bb14cb671a2c86d
barry king <barryking93@gmail.com>
2023-05-05 09:12:59 -0700

    getting classier

--- manifests/puppet/packages.pp
+++ manifests/puppet/packages.pp
@@ -1,12 +1,12 @@
 # manifests/puppet/packages.pp
 
-pocketprotector::puppet::packages::client {
+class pocketprotector::puppet::packages::client {
   package { lookup('pocketprotector::puppet::packages::client'):
     ensure => installed,
   }
 }
 
-pocketprotector::puppet::packages::server {
+class pocketprotector::puppet::packages::server {
   package { lookup('pocketprotector::puppet::packages::server'):
     ensure => installed,
   }
