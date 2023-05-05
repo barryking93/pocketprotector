f18461035a35156ef5444370c64b93df6b7106e0
barry king <barryking93@gmail.com>
2023-05-05 10:08:24 -0700

    $home wasn't a var

--- manifests/accounts.pp
+++ manifests/accounts.pp
@@ -1,20 +1,22 @@
 # manifests/accounts.pp
 #
+# account management
+#
+# crawl pocketprotector.accounts in all
 
 class pocketprotector::accounts {
   lookup('pocketprotector.accounts', undef, 'unique', undef).each |String $username, Hash $userhash| {
     #
     # accounts module doesn't like it if you specify the default home dir
     #
-    #if lookup("pocketprotector::accounts.${username}.home", undef, 'first', "/home/${username}") == "/home/${username}" {
-    #  $tmphomedir = undef
-    #}
-    #else {
-    #  $tmphomedir = $home
-    #}
+    if lookup("pocketprotector::accounts.${username}.home", undef, 'first', "/home/${username}") == "/home/${username}" {
+      $tmphomedir = undef
+    }
+    else {
+      $tmphomedir = lookup("pocketprotector::accounts.${username}.home", undef, 'first', undef)
+    }
     accounts::user { "${username}":
-      #home     => $tmphomedir,
-      home     => $home,
+      home     => $tmphomedir,
       uid      => lookup("pocketprotector::accounts.${username}.uid", undef, 'first', undef),
       groups   => lookup("pocketprotector::accounts.${username}.groups", undef, 'hash', ['users']),
       password => lookup("pocketprotector::passwords.${username}", undef, 'first', '!!'),
