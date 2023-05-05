05a2b5bb04f2200f2aded26b92f4bc7d17bbd19b
barry king <barryking93@gmail.com>
2023-05-05 09:46:27 -0700

    was it a var issue all along?

--- manifests/accounts.pp
+++ manifests/accounts.pp
@@ -6,7 +6,7 @@ class pocketprotector::accounts {
     #
     # accounts module doesn't like it if you specify the default home dir
     #
-    if lookup("pocketprotector::accounts.${name}.home", undef, 'first', "/home/${name}") == "/home/${name}" {
+    if lookup("pocketprotector::accounts.${username}.home", undef, 'first', "/home/${username}") == "/home/${username}" {
       $tmphomedir = undef
     }
     else {
@@ -14,11 +14,11 @@ class pocketprotector::accounts {
     }
     accounts::user { "${username}":
       home     => $tmphomedir,
-      uid      => lookup("pocketprotector::accounts.${name}.uid", undef, 'first', undef),
-      groups   => lookup("pocketprotector::accounts.${name}.groups", undef, 'hash', ['users']),
-      password => lookup("pocketprotector::passwords.${name}", undef, 'first', '!!'),
-      shell    => lookup("pocketprotector::accounts.${name}.shell", undef, 'first', '/bin/bash'),
-      sshkeys  => lookup("pocketprotector::accounts.${name}.sshkeys", Array[String], 'hash', []),
+      uid      => lookup("pocketprotector::accounts.${username}.uid", undef, 'first', undef),
+      groups   => lookup("pocketprotector::accounts.${username}.groups", undef, 'hash', ['users']),
+      password => lookup("pocketprotector::passwords.${username}", undef, 'first', '!!'),
+      shell    => lookup("pocketprotector::accounts.${username}.shell", undef, 'first', '/bin/bash'),
+      sshkeys  => lookup("pocketprotector::accounts.${username}.sshkeys", Array[String], 'hash', []),
     }
   }
 }
