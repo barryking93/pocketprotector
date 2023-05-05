812fd184180bcd339523189b44906f08f53f5058
barry king <barryking93@gmail.com>
2023-05-05 09:32:45 -0700

    trying to juggle more datatypes

--- manifests/accounts.pp
+++ manifests/accounts.pp
@@ -6,7 +6,7 @@ class pocketprotector::accounts {
     #
     # accounts module doesn't like it if you specify the default home dir
     #
-    if lookup("pocketprotector::accounts.${name}.home", undef, 'deep', "/home/${name}") == "/home/${name}" {
+    if lookup("pocketprotector::accounts.${name}.home", undef, 'first', "/home/${name}") == "/home/${name}" {
       $tmphomedir = undef
     }
     else {
@@ -15,10 +15,10 @@ class pocketprotector::accounts {
     accounts::user { "${username}":
       home     => $tmphomedir,
       uid      => lookup("pocketprotector::accounts.${name}.uid", undef, 'first', undef),
-      groups   => lookup("pocketprotector::accounts.${name}.groups", undef, 'deep', ['users']),
+      groups   => lookup("pocketprotector::accounts.${name}.groups", undef, 'hash', ['users']),
       password => lookup("pocketprotector::passwords.${name}", undef, 'first', '!!'),
       shell    => lookup("pocketprotector::accounts.${name}.shell", undef, 'first', '/bin/bash'),
-      sshkeys  => lookup("pocketprotector::accounts.${name}.sshkeys", Array[String], 'deep', []),
+      sshkeys  => lookup("pocketprotector::accounts.${name}.sshkeys", Array[String], 'hash', []),
     }
   }
 }
