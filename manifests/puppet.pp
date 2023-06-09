3eb48aaaea248df2596ad4a595487ef227b3fb85
barry king <barryking93@gmail.com>
2023-06-08 18:21:57 -0700

    fixing puppet client logic

--- manifests/puppet.pp
+++ manifests/puppet.pp
@@ -1,13 +1,13 @@
 # manifests/puppet.pp
 #
 
+# everyone gets the client, but only the server gets the server
 class pocketprotector::puppet {
+  include pocketprotector::puppet::client
   case $::fqdn {
     lookup('pocketprotector::puppet::server'): {
       include pocketprotector::puppet::server
     }
-    default: {
-      include pocketprotector::puppet::client
-    }
+    default: {}
   }
 }
