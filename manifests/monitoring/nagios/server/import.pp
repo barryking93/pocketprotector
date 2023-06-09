f29e7f76fbef48f5a202c5f5503280e5aed170f0
barry king <barryking93@gmail.com>
2023-06-09 14:14:03 -0700

    adding reqs back

--- manifests/monitoring/nagios/server/import.pp
+++ manifests/monitoring/nagios/server/import.pp
@@ -5,27 +5,27 @@
 class pocketprotector::monitoring::nagios::server::import {
 
   Nagios_command <<||>> {
-    notify  => lookup('pocketprotector::monitoring::nagios::service::server'),
-#    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
+    notify  => Service[lookup('pocketprotector::monitoring::nagios::service::server')],
+    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
   }
   Nagios_contact <<||>> {
-    notify  => lookup('pocketprotector::monitoring::nagios::service::server'),
-#    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
+    notify  => Service[lookup('pocketprotector::monitoring::nagios::service::server')],
+    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
   }
   Nagios_host <<||>> {
-    notify  => lookup('pocketprotector::monitoring::nagios::service::server'),
-#    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
+    notify  => Service[lookup('pocketprotector::monitoring::nagios::service::server')],
+    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
   }
   Nagios_hostgroup <<||>> {
-    notify  => lookup('pocketprotector::monitoring::nagios::service::server'),
-#    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
+    notify  => Service[lookup('pocketprotector::monitoring::nagios::service::server')],
+    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
   }
   Nagios_service <<||>> {
-    notify  => lookup('pocketprotector::monitoring::nagios::service::server'),
-#    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
+    notify  => Service[lookup('pocketprotector::monitoring::nagios::service::server')],
+    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
   }
   Nagios_timeperiod <<||>> {
-    notify  => lookup('pocketprotector::monitoring::nagios::service::server'),
-#    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
+    notify  => Service[lookup('pocketprotector::monitoring::nagios::service::server')],
+    require => Package[lookup('pocketprotector::monitoring::nagios::packages::server')],
   }
 }
