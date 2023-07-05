cf8beea97fe3358814bb5f218ce2892a779484ea
barry king <barryking93@gmail.com>
2023-07-05 10:45:39 -0700

    expand repository methods to include package repos from heira

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,6 +1,12 @@
 # manifests/packages.pp
-
+#
 class pocketprotector::packages {
+  include pocketprotector::packages::repositories
+  include pocketprotector::packages::install
+}
+
+# package installer
+class pocketprotector::packages::install {
   case lookup('pocketprotector::packages', undef, 'deep', false) {
     false: {}
     default: {
@@ -17,6 +23,28 @@ class pocketprotector::packages {
         lookup('pocketprotector::packages::latest', undef, 'deep', undef):
           ensure => latest;
       }
+    }
+  }
+}
+
+# build repository list & pass to appropriate module
+class pocketprotector::packages::repositories {
+  case lookup('pocketprotector::packages::provider') {
+    'apt': {
+      class {'apt':
+        purge => {
+          'sources.list'   => true,
+          'sources.list.d' => true
+        };
+      }
+      apt::source {
+        lookup('pocketprotector::packages::repositories',undef,deep,undef)
+      }
+    }
+    'dnf': {}
+    'zypper': {}
+    default: {
+      notify{'the package repository for your OS is not (yet?) supported'}:
     }
   }
 }
