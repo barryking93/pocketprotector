06f93a4e42d38cdb2e6bb9546a852b0037288633
barry king <barryking93@gmail.com>
2023-07-05 12:12:57 -0700

    breaking out apt

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -31,27 +31,31 @@ class pocketprotector::packages::install {
 class pocketprotector::packages::repositories {
   case lookup('pocketprotector::packages::provider') {
     'apt': {
-      class {'apt':
-        purge => {
-          'sources.list'   => true,
-          'sources.list.d' => true
-        },
-      }
-
-      lookup('pocketprotector::packages::repositories',undef,deep,undef).each | String $aptrepo, Hash $aptrepohash | {
-        apt::source { $aptrepo:
-          location => lookup("pocketprotector::packages::repositories.${aptrepo}.location",undef,deep,undef),
-          release  => lookup("pocketprotector::packages::repositories.${aptrepo}.release",undef,deep,undef),
-          repos    => lookup("pocketprotector::packages::repositories.${aptrepo}.release",undef,deep,undef),
-          key      => {
-            'id'     => lookup("pocketprotector::packages::repositories.${aptrepo}.key.id",undef,deep,undef),
-            'source' => lookup("pocketprotector::packages::repositories.${aptrepo}.key.source",undef,deep,undef),
-          }
-        }
-      }
+      include pocketprotector::packages::repositories::apt
     }
     default: {
       notify{'the package repository for your OS is not (yet?) supported':}
+    }
+  }
+}
+
+class pocketprotector::packages::provider::apt {
+  class {'apt':
+    purge => {
+      'sources.list'   => true,
+      'sources.list.d' => true
+    },
+  }
+
+  lookup('pocketprotector::packages::repositories',undef,deep,undef).each | String $aptrepo, Hash $aptrepohash | {
+    apt::source { $aptrepo:
+      location => lookup("pocketprotector::packages::repositories.${aptrepo}.location",undef,deep,undef),
+      release  => lookup("pocketprotector::packages::repositories.${aptrepo}.release",undef,deep,undef),
+      repos    => lookup("pocketprotector::packages::repositories.${aptrepo}.release",undef,deep,undef),
+      key      => {
+        'id'     => lookup("pocketprotector::packages::repositories.${aptrepo}.key.id",undef,deep,undef),
+        'source' => lookup("pocketprotector::packages::repositories.${aptrepo}.key.source",undef,deep,undef),
+      }
     }
   }
 }
