54409fecf8c054eabb7c2b82c4868206847c738d
barry king <barryking93@gmail.com>
2023-07-05 11:21:26 -0700

    helps if packages gets included, eh?

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@
 class pocketprotector {
   include pocketprotector::accounts
   include pocketprotector::puppet
+  include pocketprotector::packages
   include pocketprotector::monitoring
   include pocketprotector::mta
 }
