b95d0385cadb6db9b4f26633256ab8ea90869432
barry king <barryking93@gmail.com>
2023-07-17 15:33:58 -0700

    docs added to init.pp, reordered

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,23 @@
 # manifests/init.pp
+#
+#
 
 class pocketprotector {
+  # order here matters:
+  #
+  # packages first to set up proper repos
   include pocketprotector::packages
+  # accounts to establish fundamental owners for needed later ownership
   include pocketprotector::accounts
-  include pocketprotector::etc  
+  # files for baseline file rollouts
+  include pocketprotector::file
+  # etc to roll out custom facts/etc
+  include pocketprotector::etc
+  # puppet to ensure that this runs a second time
   include pocketprotector::puppet
+  # firewall, when supported
+  #include pocketprotector::firewall
+  # everything beyond here is alphabetical
   include pocketprotector::monitoring
   include pocketprotector::mta
 }
