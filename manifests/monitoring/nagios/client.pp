5b93e7fb0354453965aefce2dc068bf3b04a24be
barry king <barryking93@gmail.com>
2023-08-09 14:42:14 -0700

    changing if

--- manifests/monitoring/nagios/client.pp
+++ manifests/monitoring/nagios/client.pp
@@ -1,7 +1,7 @@
 #
 
 class pocketprotector::monitoring::nagios::client {
-  if lookup('pocketprotector::monitoring::enable') == true {
+  if lookup('pocketprotector::monitoring::enable',undef,undef,false) {
     # install client packages
     package {
       lookup('pocketprotector::monitoring::nagios::packages::client', undef, 'deep', undef):
