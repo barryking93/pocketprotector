413faf85faad9a7503018f8de8e77361642dbe5d
barry king <barryking93@gmail.com>
2023-08-18 17:28:35 -0700

    adding zypper support

--- manifests/packages/apt.pp
+++ manifests/packages/apt.pp
@@ -1,46 +1,4 @@
-# manifests/packages.pp
-#
-class pocketprotector::packages {
-  include pocketprotector::packages::repositories
-  include pocketprotector::packages::install
-}
-
-# package installer
-class pocketprotector::packages::install {
-  case lookup('pocketprotector::packages', undef, 'deep', false) {
-    false: {}
-    default: {
-      package {
-        lookup('pocketprotector::packages', undef, 'deep', undef):
-          ensure => installed;
-      }
-    }
-  }
-  case lookup('pocketprotector::packages::latest', undef, 'deep', false) {
-    false: {}
-    default: {
-      package {
-        lookup('pocketprotector::packages::latest', undef, 'deep', undef):
-          ensure => latest;
-      }
-    }
-  }
-}
-
-# build repository list & pass to appropriate module
-class pocketprotector::packages::repositories {
-  case lookup('pocketprotector::packages::provider') {
-    'apt': {
-      include pocketprotector::packages::repositories::apt
-    }
-    'zypper': {
-      include pocketprotector::packages::repositories::zypper
-    }
-    default: {
-      notify{'pocketprotector::packages::repositories: the package repository for your OS is not (yet?) supported':}
-    }
-  }
-}
+# manifests/packages/apt.pp
 
 class pocketprotector::packages::repositories::apt {
   include pocketprotector::packages::repositories::apt::init
