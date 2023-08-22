2f4073c3cd9630e1160204f4f488d7d9a29f1b9a
barry king <barryking93@gmail.com>
2023-08-22 09:43:48 -0700

    seeing if the var works here

--- manifests/puppet/server.pp
+++ manifests/puppet/server.pp
@@ -63,11 +63,10 @@ class pocketprotector::puppet::server::puppetboard {
 
 class pocketprotector::puppet::server::puppetdb {
   class {
-    'puppetdb':
-      ssl_deploy_certs => true;
     'puppetdb::server':
       database_host => lookup('pocketprotector::puppet::puppetdb::database::hostname',undef,undef,'localhost'),
-      java_args     => { '-Xmx' => lookup('pocketprotector::puppet::puppetdb::database::maxram',undef,undef,'2g') };
+      java_args     => { '-Xmx' => lookup('pocketprotector::puppet::puppetdb::database::maxram',undef,undef,'2g') },
+      ssl_deploy_certs => true;
     'puppetdb::database::postgresql':
       listen_addresses => lookup('pocketprotector::puppet::puppetdb::database::listen_addresses',undef,undef,'localhost'),
       postgres_version => lookup('pocketprotector::puppet::puppetdb::database::version',undef,undef,undef),
