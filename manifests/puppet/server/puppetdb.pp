77c3760c31f2ba22a90f6cde848f0ea61e81e2a6
barry king <barryking93@gmail.com>
2023-10-14 08:21:53 -0700

    reversion

--- manifests/puppet/server/puppetdb.pp
+++ manifests/puppet/server/puppetdb.pp
@@ -7,11 +7,12 @@
 class pocketprotector::puppet::server::puppetdb {
   class {
     'puppetdb::server':
-    #  database_host      => lookup('pocketprotector::puppet::puppetdb::database::hostname',undef,undef,'localhost'),
+      database_host      => lookup('pocketprotector::puppet::puppetdb::database::hostname',undef,undef,'localhost'),
       java_args          => { '-Xmx' => lookup('pocketprotector::puppet::puppetdb::database::maxram',undef,undef,'2g') },
       ssl_set_cert_paths => true;
-    #'puppetdb::database::postgresql':
-    #  listen_addresses => lookup('pocketprotector::puppet::puppetdb::database::listen_addresses',undef,undef,'localhost'),
-    #  postgres_version => lookup('pocketprotector::puppet::puppetdb::database::version',undef,undef,undef),
+    'puppetdb::database::postgresql':
+      listen_addresses => lookup('pocketprotector::puppet::puppetdb::database::listen_addresses',undef,undef,'localhost'),
+      postgres_version => lookup('pocketprotector::puppet::puppetdb::database::version',undef,undef,undef),
+    'puppetdb::master::config' :
   }
 }
