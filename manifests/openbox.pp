7d395cab348c1a3a4075b457e8ef9d8647abcace
barry king <barryking93@gmail.com>
2023-10-18 16:14:09 -0700

    making use of new files/templates functions

--- manifests/openbox.pp
+++ manifests/openbox.pp
@@ -1,31 +1,15 @@
 # manifests/openbox.pp
 
 class pocketprotector::openbox {
-  if lookup('pocketprotector::openbox::enabled',undef,deep,false) {
+  if lookup('pocketprotector::openbox::enabled',undef,'deep',false) {
     # deploy packages
     package {
-      lookup('pocketprotector::openbox::packages',undef,deep,undef):
+      lookup('pocketprotector::openbox::packages',undef,'deep',undef):
         ensure => installed;
     }
     # configure files
-    lookup('pocketprotector::openbox::files', undef, 'deep', undef).each |String $filename, Hash $filehash| {
-      #notify {"pocketprotector::openbox: debug file for ${filename}":}
-
-      file {
-        $filename:
-          path                    => lookup("pocketprotector::openbox::files.${filename}.path", undef, 'deep', undef),
-          source                  => lookup("pocketprotector::openbox::files.${filename}.source", undef, 'deep', undef),
-      }
-    }
+    pocketprotector::files(lookup('pocketprotector::openbox::files', undef, 'deep', undef)
     # configure templates
-    lookup('pocketprotector::openbox::files::templates', undef, 'deep', undef).each |String $filename, Hash $filehash| {
-      #notify {"pocketprotector::openbox: debug file template for ${filename}":}
-
-      file {
-        $filename:
-          path                    => lookup("pocketprotector::openbox::files::templates.${filename}.path", undef, 'deep', undef),
-          content                 => template(lookup("pocketprotector::openbox::files::templates.${filename}.content", undef, 'deep', undef)),
-      }
-    }
+    pocketprotector::files(lookup('pocketprotector::openbox::files::templates', undef, 'deep', undef)
   }
 }
