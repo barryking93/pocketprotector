4ba631b120535c87bf0634d3628df5d1e07d0840
barry king <barryking93@gmail.com>
2023-10-18 15:04:22 -0700

    revamping account lookup

--- manifests/accounts.pp
+++ manifests/accounts.pp
@@ -3,22 +3,30 @@
 # account management
 
 #
+# custom content type to deal with accounts
+#
+
+define pocketprotector::accounts (
+  Hash $usershash
+  ){
+  $usershash.each |String $username, Hash $userhash| {
+    #notify {"pocketprotector::accounts: account creation for for ${username}":}
+
+    accounts::user {
+      $username:
+        home     => lookup("pocketprotector::accounts.${username}.home", undef, 'first', "/home/${username}"),
+        uid      => lookup("pocketprotector::accounts.${username}.uid", undef, 'first', undef),
+        groups   => lookup("pocketprotector::accounts.${username}.groups", undef, 'deep', ['users']),
+        password => lookup("pocketprotector::passwords.${username}", undef, 'first', '!!'),
+        shell    => lookup("pocketprotector::accounts.${username}.shell", undef, 'first', '/bin/bash'),
+        sshkeys  => lookup("pocketprotector::accounts.${username}.sshkeys", Array[String], 'deep', []),
+    }
+  }
+}
+
+#
 # crawl pocketprotector.accounts in hieradata for user list
 #
 class pocketprotector::accounts {
-  if lookup('pocketprotector::accounts', undef, 'deep', false) {
-    lookup('pocketprotector::accounts', undef, 'deep', undef).each |String $username, Hash $userhash| {
-      #notify {"pocketprotector::accounts: account creation for for ${username}":}
-
-      accounts::user {
-        $username:
-          home     => lookup("pocketprotector::accounts.${username}.home", undef, 'first', "/home/${username}"),
-          uid      => lookup("pocketprotector::accounts.${username}.uid", undef, 'first', undef),
-          groups   => lookup("pocketprotector::accounts.${username}.groups", undef, 'deep', ['users']),
-          password => lookup("pocketprotector::passwords.${username}", undef, 'first', '!!'),
-          shell    => lookup("pocketprotector::accounts.${username}.shell", undef, 'first', '/bin/bash'),
-          sshkeys  => lookup("pocketprotector::accounts.${username}.sshkeys", Array[String], 'deep', []),
-      }
-    }
-  }
+  pocketprotector::accounts {lookup('pocketprotector::accounts', undef, 'deep', false):}
 }
