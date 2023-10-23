aedead587719df0ac42e86af10ad97bb4d358b6a
barry king <barryking93@gmail.com>
2023-10-23 16:05:53 -0700

    fixing notifies

--- manifests/files.pp
+++ manifests/files.pp
@@ -50,7 +50,7 @@ define pocketprotector::files::parse (
       }
     }
   } else {
-    notify{"pocketprotector::files::parse lookup filed for ${filename}"}
+    notify{"pocketprotector::files::parse lookup filed for ${filesyaml}":}
   }
 }
 
@@ -58,47 +58,52 @@ define pocketprotector::files::parse (
 define pocketprotector::files::templates::parse (
   String $filesyaml,
 ){
-  lookup($filesyaml, undef, 'deep', undef).each |String $filename, Hash $filehash| {
-    #notify {"pocketprotector::files::templates debug file for ${filename}":}
+  if lookup($filesyaml, undef, 'deep', false) {
+    lookup($filesyaml, undef, 'deep', undef).each |String $filename, Hash $filehash| {
+      #notify {"pocketprotector::files::templates debug file for ${filename}":}
 
-    file {
-      $filename:
-        path                    => lookup("pocketprotector::files::templates.${filename}.path", undef, 'deep', undef),
-        ensure                  => lookup("pocketprotector::files::templates.${filename}.ensure", undef, 'deep', undef),
-        backup                  => lookup("pocketprotector::files::templates.${filename}.backup", undef, 'deep', undef),
-        checksum                => lookup("pocketprotector::files::templates.${filename}.checksum", undef, 'deep', undef),
-        checksum_value          => lookup("pocketprotector::files::templates.${filename}.checksum_value", undef, 'deep', undef),
-        content                 => template(lookup("pocketprotector::files::templates.${filename}.content", undef, 'deep', undef)),
-        ctime                   => lookup("pocketprotector::files::templates.${filename}.ctime", undef, 'deep', undef),
-        force                   => lookup("pocketprotector::files::templates.${filename}.force", undef, 'deep', undef),
-        group                   => lookup("pocketprotector::files::templates.${filename}.group", undef, 'deep', undef),
-        ignore                  => lookup("pocketprotector::files::templates.${filename}.ignore", undef, 'deep', undef),
-        links                   => lookup("pocketprotector::files::templates.${filename}.links", undef, 'deep', undef),
-        mode                    => lookup("pocketprotector::files::templates.${filename}.mode", undef, 'deep', undef),
-        mtime                   => lookup("pocketprotector::files::templates.${filename}.require", undef, 'deep', undef),
-        notify                  => lookup("pocketprotector::files::templates.${filename}.notify", undef, 'deep', undef),
-        owner                   => lookup("pocketprotector::files::templates.${filename}.owner", undef, 'deep', undef),
-        provider                => lookup("pocketprotector::files::templates.${filename}.provider", undef, 'deep', undef),
-        purge                   => lookup("pocketprotector::files::templates.${filename}.purge", undef, 'deep', undef),
-        recurse                 => lookup("pocketprotector::files::templates.${filename}.recurse", undef, 'deep', undef),
-        recurselimit            => lookup("pocketprotector::files::templates.${filename}.recurselimit", undef, 'deep', undef),
-        replace                 => lookup("pocketprotector::files::templates.${filename}.replace", undef, 'deep', undef),
-        require                 => lookup("pocketprotector::files::templates.${filename}.require", undef, 'deep', undef),
-        selinux_ignore_defaults => lookup("pocketprotector::files::templates.${filename}.selinux_ignore_defaults", undef, 'deep', undef),
-        selrange                => lookup("pocketprotector::files::templates.${filename}.selrange", undef, 'deep', undef),
-        selrole                 => lookup("pocketprotector::files::templates.${filename}.selrole", undef, 'deep', undef),
-        seltype                 => lookup("pocketprotector::files::templates.${filename}.seltype", undef, 'deep', undef),
-        seluser                 => lookup("pocketprotector::files::templates.${filename}.seluser", undef, 'deep', undef),
-        show_diff               => lookup("pocketprotector::files::templates.${filename}.show_diff", undef, 'deep', undef),
-        #source                  => lookup("pocketprotector::files::templates.${filename}.source", undef, 'deep', undef),
-        source_permissions      => lookup("pocketprotector::files::templates.${filename}.source_permissions", undef, 'deep', undef),
-        sourceselect            => lookup("pocketprotector::files::templates.${filename}.sourceselect", undef, 'deep', undef),
-        target                  => lookup("pocketprotector::files::templates.${filename}.target", undef, 'deep', undef),
-        type                    => lookup("pocketprotector::files::templates.${filename}.type", undef, 'deep', undef),
-        validate_cmd            => lookup("pocketprotector::files::templates.${filename}.validate_cmd", undef, 'deep', undef),
-        validate_replacement    => lookup("pocketprotector::files::templates.${filename}.validate_replacement", undef, 'deep', undef);
+      file {
+        $filename:
+          path                    => lookup("pocketprotector::files::templates.${filename}.path", undef, 'deep', undef),
+          ensure                  => lookup("pocketprotector::files::templates.${filename}.ensure", undef, 'deep', undef),
+          backup                  => lookup("pocketprotector::files::templates.${filename}.backup", undef, 'deep', undef),
+          checksum                => lookup("pocketprotector::files::templates.${filename}.checksum", undef, 'deep', undef),
+          checksum_value          => lookup("pocketprotector::files::templates.${filename}.checksum_value", undef, 'deep', undef),
+          content                 => template(lookup("pocketprotector::files::templates.${filename}.content", undef, 'deep', undef)),
+          ctime                   => lookup("pocketprotector::files::templates.${filename}.ctime", undef, 'deep', undef),
+          force                   => lookup("pocketprotector::files::templates.${filename}.force", undef, 'deep', undef),
+          group                   => lookup("pocketprotector::files::templates.${filename}.group", undef, 'deep', undef),
+          ignore                  => lookup("pocketprotector::files::templates.${filename}.ignore", undef, 'deep', undef),
+          links                   => lookup("pocketprotector::files::templates.${filename}.links", undef, 'deep', undef),
+          mode                    => lookup("pocketprotector::files::templates.${filename}.mode", undef, 'deep', undef),
+          mtime                   => lookup("pocketprotector::files::templates.${filename}.require", undef, 'deep', undef),
+          notify                  => lookup("pocketprotector::files::templates.${filename}.notify", undef, 'deep', undef),
+          owner                   => lookup("pocketprotector::files::templates.${filename}.owner", undef, 'deep', undef),
+          provider                => lookup("pocketprotector::files::templates.${filename}.provider", undef, 'deep', undef),
+          purge                   => lookup("pocketprotector::files::templates.${filename}.purge", undef, 'deep', undef),
+          recurse                 => lookup("pocketprotector::files::templates.${filename}.recurse", undef, 'deep', undef),
+          recurselimit            => lookup("pocketprotector::files::templates.${filename}.recurselimit", undef, 'deep', undef),
+          replace                 => lookup("pocketprotector::files::templates.${filename}.replace", undef, 'deep', undef),
+          require                 => lookup("pocketprotector::files::templates.${filename}.require", undef, 'deep', undef),
+          selinux_ignore_defaults => lookup("pocketprotector::files::templates.${filename}.selinux_ignore_defaults", undef, 'deep', undef),
+          selrange                => lookup("pocketprotector::files::templates.${filename}.selrange", undef, 'deep', undef),
+          selrole                 => lookup("pocketprotector::files::templates.${filename}.selrole", undef, 'deep', undef),
+          seltype                 => lookup("pocketprotector::files::templates.${filename}.seltype", undef, 'deep', undef),
+          seluser                 => lookup("pocketprotector::files::templates.${filename}.seluser", undef, 'deep', undef),
+          show_diff               => lookup("pocketprotector::files::templates.${filename}.show_diff", undef, 'deep', undef),
+          #source                  => lookup("pocketprotector::files::templates.${filename}.source", undef, 'deep', undef),
+          source_permissions      => lookup("pocketprotector::files::templates.${filename}.source_permissions", undef, 'deep', undef),
+          sourceselect            => lookup("pocketprotector::files::templates.${filename}.sourceselect", undef, 'deep', undef),
+          target                  => lookup("pocketprotector::files::templates.${filename}.target", undef, 'deep', undef),
+          type                    => lookup("pocketprotector::files::templates.${filename}.type", undef, 'deep', undef),
+          validate_cmd            => lookup("pocketprotector::files::templates.${filename}.validate_cmd", undef, 'deep', undef),
+          validate_replacement    => lookup("pocketprotector::files::templates.${filename}.validate_replacement", undef, 'deep', undef);
+        }
     }
+  } else {
+    notify{"pocketprotector::files::templates::parse lookup failed for ${filesyaml}":}
   }
+
 }
 #
 # feed pocketprotector::files and pocketprotector::files::tempaltes to appropriate generators
