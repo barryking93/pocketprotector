1d1b9b3ec5e3a62aac9e0301cb3dd5a3b62fea7c
barry king <barryking93@gmail.com>
2023-10-24 10:24:51 -0700

    adding locked to accounts

--- manifests/accounts.pp
+++ manifests/accounts.pp
@@ -19,6 +19,7 @@ define pocketprotector::accounts::parse (
         home     => lookup("pocketprotector::accounts.${username}.home", undef, 'first', "/home/${username}"),
         uid      => lookup("pocketprotector::accounts.${username}.uid", undef, 'first', undef),
         groups   => lookup("pocketprotector::accounts.${username}.groups", undef, 'deep', ['users']),
+        locked   => lookup("pocketprotector::accounts.${username}.locked", undef, 'first', undef),
         password => lookup("pocketprotector::passwords.${username}", undef, 'first', '!!'),
         shell    => lookup("pocketprotector::accounts.${username}.shell", undef, 'first', '/bin/bash'),
         sshkeys  => lookup("pocketprotector::accounts.${username}.sshkeys", Array[String], 'deep', []),
