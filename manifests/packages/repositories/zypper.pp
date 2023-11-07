01be938e07b096fd1a9881c89a6c249c4ba06469
barry king <barryking93@gmail.com>
2023-11-07 12:23:28 -0800

    fixing spacing, which revealed bracket issue

--- manifests/packages/repositories/zypper.pp
+++ manifests/packages/repositories/zypper.pp
@@ -3,27 +3,30 @@
 # zypper package (and repository) support
 #
 
-define pocketprotector::packages::repositories::zypper::repositories::parse (
+define pocketprotector::packages::repositories::zypper::repoparse (
   String $sourceyaml = $name,
 ){
   if lookup($sourceyaml,undef,'deep',false) {    
-   lookup($sourceyaml,undef,'deep',undef).each | String $zypprepo, Hash $zypprepohash | {
-    zypprepo { $zypprepo:
-      ensure        => lookup("${soureceyaml}.${zypprepo}.ensure",undef,deep,undef),
-      baseurl       => lookup("${soureceyaml}.${zypprepo}.baseurl",undef,deep,undef),
-      enabled       => lookup("${soureceyaml}.${zypprepo}.enabled",undef,deep,undef),
-      autorefresh   => lookup("${soureceyaml}.${zypprepo}.enabled",undef,deep,undef),
-      name          => lookup("${soureceyaml}.${zypprepo}.name",undef,deep,undef),
-      gpgcheck      => lookup("${soureceyaml}.${zypprepo}.gpgcheck",undef,deep,undef),
-      repo_gpgcheck => lookup("${soureceyaml}.${zypprepo}.repo_gpgcheck",undef,deep,undef),
-      pkg_gpgcheck  => lookup("${soureceyaml}.${zypprepo}.pgk_gpgcheck",undef,deep,undef),
-      priority      => lookup("${soureceyaml}.${zypprepo}.priority",undef,deep,undef),
-      keeppackages  => lookup("${soureceyaml}.${zypprepo}.keeppackages",undef,deep,undef),
-      type          => lookup("${soureceyaml}.${zypprepo}.type",undef,deep,undef),
+    lookup($sourceyaml,undef,'deep',undef).each | String $zypprepo, Hash $zypprepohash | {
+      zypprepo { $zypprepo:
+        ensure        => lookup("${soureceyaml}.${zypprepo}.ensure",undef,deep,undef),
+        baseurl       => lookup("${soureceyaml}.${zypprepo}.baseurl",undef,deep,undef),
+        enabled       => lookup("${soureceyaml}.${zypprepo}.enabled",undef,deep,undef),
+        autorefresh   => lookup("${soureceyaml}.${zypprepo}.enabled",undef,deep,undef),
+        name          => lookup("${soureceyaml}.${zypprepo}.name",undef,deep,undef),
+        gpgcheck      => lookup("${soureceyaml}.${zypprepo}.gpgcheck",undef,deep,undef),
+        repo_gpgcheck => lookup("${soureceyaml}.${zypprepo}.repo_gpgcheck",undef,deep,undef),
+        pkg_gpgcheck  => lookup("${soureceyaml}.${zypprepo}.pgk_gpgcheck",undef,deep,undef),
+        priority      => lookup("${soureceyaml}.${zypprepo}.priority",undef,deep,undef),
+        keeppackages  => lookup("${soureceyaml}.${zypprepo}.keeppackages",undef,deep,undef),
+        type          => lookup("${soureceyaml}.${zypprepo}.type",undef,deep,undef),
+      }
     }
+  } else {
+    notify{"pocketprotector::packages::repositories::zypper::repoparse: lookup failed for ${sourceyaml}":}
   }
 }
 
 class pocketprotector::packages::repositories::zypper {
-  pocketprotector::packages::repositories::zypper::repositories::parse{'pocketprotector::packages::repositories':}
+  pocketprotector::packages::repositories::zypper::repoparse{'pocketprotector::packages::repositories':}
 }
