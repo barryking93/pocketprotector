f32a5813136348944599c0995becc2b3732b96c2
barry king <barryking93@gmail.com>
2023-12-05 13:04:51 -0800

    fixing a var

--- manifests/puppet/server/puppetboard.pp
+++ manifests/puppet/server/puppetboard.pp
@@ -13,20 +13,22 @@ class pocketprotector::puppet::server::puppetboard {
 }
 
 class pocketprotector::puppet::server::puppetboard::docker {
-  include docker
+    include docker
 
-  docker::image { 'ghcr.io/voxpupuli/puppetboard': }
+    $pb_key = lookup('pocketprotector::puppet::server::puppetboard::secret_key')
 
-  docker::run { 'puppetboard':
-    image => 'ghcr.io/voxpupuli/puppetboard',
-    env   => [
-      'PUPPETDB_HOST=127.0.0.1',
-      'PUPPETDB_PORT=8080',
-      'PUPPETBOARD_PORT=8088',
-      'SECRET_KEY'=lookup('pocketprotector::puppet::server::puppetboard::secret_key'),
-    ],
-    net   => 'host',
-  }
+    docker::image { 'ghcr.io/voxpupuli/puppetboard': }
+
+    docker::run { 'puppetboard':
+        image => 'ghcr.io/voxpupuli/puppetboard',
+        env   => [
+            'PUPPETDB_HOST=127.0.0.1',
+            'PUPPETDB_PORT=8080',
+            'PUPPETBOARD_PORT=8088',
+            "SECRET_KEY=${pb_key}",
+        ],
+        net   => 'host',
+    }
 }
 
 class pocketprotector::puppet::server::puppetboard::native {
