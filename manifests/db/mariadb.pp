77f39574001dbfce909e01855a9d2df5dd765c0d
barry king <barryking93@gmail.com>
2023-12-19 15:26:30 -0800

    fixing duplicate code in maridab

--- manifests/db/mariadb.pp
+++ manifests/db/mariadb.pp
@@ -1,9 +1,6 @@
 # manifests/db/mariadb.pp
 
 class pocketprotector::db::maridb {
-  if lookup('pocketprotector::db::mariadb::enabled',undef,'deep',false) {
-    include pocketprotector::db::mariadb
-  }
   pocketprotector::packages::parse{'pocketprotector::db::mariadb::packages':}
 }
 
