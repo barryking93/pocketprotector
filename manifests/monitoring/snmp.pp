5c973cde35ec8de7714b4152848a4ea4576b3749
barry king <barryking93@gmail.com>
2023-12-27 11:25:57 -0800

    copypasta error

--- manifests/monitoring/snmp.pp
+++ manifests/monitoring/snmp.pp
@@ -5,6 +5,6 @@ class pocketprotector::monitoring::snmp {
   if lookup('pocketprotector::monitoring::snmp::accounts',undef,'first',false) {
     pocketprotector::accounts::parse{'pocketprotector::monitoring::snmp::accounts':}
   }
-  pocketprotector::files::packages::parse{"pocketprotector::monitoring::snmp::packages":}
+  pocketprotector::packages::parse{"pocketprotector::monitoring::snmp::packages":}
   pocketprotector::files::templates::parse{"pocketprotector::monitoring::snmp::templates":}
 }
