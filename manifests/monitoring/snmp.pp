908ce17005cb127d1623aa27d725c91a12f32d04
barry king <barryking93@gmail.com>
2023-12-27 10:46:39 -0800

    derp

--- manifests/monitoring/snmp.pp
+++ manifests/monitoring/snmp.pp
@@ -6,11 +6,4 @@ class pocketprotector::monitoring::snmp {
     pocketprotector::accounts::parse{'pocketprotector::monitoring::snmp::accounts':}
   }
   pocketprotector::files::templates::parse{"pocketprotector::monitoring::snmp::templates":}
-  class { 'snmp':
-    agentaddress => lookup('pocketprotector::monitoring::snmp::agentaddress',undef,'first',undef),
-    contact      => lookup('pocketprotector::monitoring::snmp::contact',undef,'first',undef),
-    location     => lookup('pocketprotector::monitoring::snmp::location',undef,'first',undef),
-    ro_community => lookup('pocketprotector::monitoring::snmp::ro_community',undef,'first',undef),
-    ro_network   => lookup('pocketprotector::monitoring::snmp::ro_network',undef,'first',undef),
-  }
 }
