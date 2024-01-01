d912f7d53b0e181c5e3813eae19a36ef6f2af922
barry king <barryking93@gmail.com>
2024-01-01 13:06:58 -0800

    fixing/standardizing yaml paths

--- manifests/packages/updates/apt.pp
+++ manifests/packages/updates/apt.pp
@@ -3,13 +3,13 @@
 # apt automatic update support
 #
 
-class pocketprotector::packages::updates::apt {
-  if lookup('pocketprotector::packages::update::apt::packages',undef,'deep',false) {
-    pocketprotector::packages::parse{'pocketprotector::packages::update::apt::packages':}
+class pocketprotector::packages::updatess::apt {
+  if lookup('pocketprotector::packages::updates::apt::packages',undef,'deep',false) {
+    pocketprotector::packages::parse{'pocketprotector::packages::updates::apt::packages':}
   }
 
-  if lookup('pocketprotector::packages::update::apt::templates',undef,'deep',false) {
-    pocketprotector::files::templates::parse{'pocketprotector::packages::update::apt::templates':}
+  if lookup('pocketprotector::packages::updates::apt::files::templates',undef,'deep',false) {
+    pocketprotector::files::templates::parse{'pocketprotector::packages::updates::apt::files::templates':}
   }
 }
 
