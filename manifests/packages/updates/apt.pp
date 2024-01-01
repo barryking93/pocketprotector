3908eb13b61f0a8722c51ed8c2d0dfb2bdfa0501
barry king <barryking93@gmail.com>
2024-01-01 13:00:14 -0800

    not a function, but a class

--- manifests/packages/updates/apt.pp
+++ manifests/packages/updates/apt.pp
@@ -3,7 +3,7 @@
 # apt automatic update support
 #
 
-define pocketprotector::packages::updates::apt (){
+class pocketprotector::packages::updates::apt {
   if lookup('pocketprotector::packages::update::apt::packages',undef,'deep',false) {
     pocketprotector::packages::parse{'pocketprotector::packages::update::apt::packages':}
   }
