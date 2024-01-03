e65522f1d5845ffe7da18d3312d88e0f270c8c0e
barry king <barryking93@gmail.com>
2024-01-03 13:10:11 -0800

    only enforce groups if exists

--- manifests/groups.pp
+++ manifests/groups.pp
@@ -11,24 +11,26 @@
 define pocketprotector::groups::parse (
   String $groupyaml = $name,
   ){
-  lookup($groupyaml, undef, 'deep', false).each |String $groupname, Hash $grouphash| {
-    #notify {"pocketprotector::groups: group creation for for ${groupname}":}
+  if lookup($groupyaml, undef, 'deep', false)) {
+    lookup($groupyaml, undef, 'deep', false).each |String $groupname, Hash $grouphash| {
+      #notify {"pocketprotector::groups: group creation for for ${groupname}":}
 
-    # https://www.puppet.com/docs/puppet/8/types/group.html
-    group {
-      $groupname:
-        allowdupe            => lookup("${usersyaml}.${username}.allowdupe", undef, 'first', undef),
-        attribute_membership => lookup("${usersyaml}.${username}.attribute_membership", undef, 'first', undef),
-        attributes           => lookup("${usersyaml}.${username}.attributes", undef, 'first', undef),
-        auth_membership      => lookup("${usersyaml}.${username}.auth_membership", undef, 'first', undef),
-        ensure               => lookup("${usersyaml}.${username}.ensure", undef, 'first', undef),
-        forcelocal           => lookup("${usersyaml}.${username}.forcelocal", undef, 'first', undef),
-        gid                  => lookup("${usersyaml}.${username}.gid", undef, 'first', undef),
-        ia_load_module       => lookup("${usersyaml}.${username}.ia_load_module", undef, 'first', undef),
-        members              => lookup("${usersyaml}.${username}.members", undef, 'first', undef),
-        name                 => lookup("${usersyaml}.${username}.name", undef, 'first', undef),
-        provider             => lookup("${usersyaml}.${username}.provider", undef, 'first', undef),
-        system               => lookup("${usersyaml}.${username}.system", undef, 'first', undef),
+      # https://www.puppet.com/docs/puppet/8/types/group.html
+      group {
+        $groupname:
+          allowdupe            => lookup("${usersyaml}.${username}.allowdupe", undef, 'first', undef),
+          attribute_membership => lookup("${usersyaml}.${username}.attribute_membership", undef, 'first', undef),
+          attributes           => lookup("${usersyaml}.${username}.attributes", undef, 'first', undef),
+          auth_membership      => lookup("${usersyaml}.${username}.auth_membership", undef, 'first', undef),
+          ensure               => lookup("${usersyaml}.${username}.ensure", undef, 'first', undef),
+          forcelocal           => lookup("${usersyaml}.${username}.forcelocal", undef, 'first', undef),
+          gid                  => lookup("${usersyaml}.${username}.gid", undef, 'first', undef),
+          ia_load_module       => lookup("${usersyaml}.${username}.ia_load_module", undef, 'first', undef),
+          members              => lookup("${usersyaml}.${username}.members", undef, 'first', undef),
+          name                 => lookup("${usersyaml}.${username}.name", undef, 'first', undef),
+          provider             => lookup("${usersyaml}.${username}.provider", undef, 'first', undef),
+          system               => lookup("${usersyaml}.${username}.system", undef, 'first', undef),
+      }
     }
   }
 }
