8d5e9de8a9fddafa159e0814e0b210acd2d9e0a5
barry king <barryking93@gmail.com>
2024-01-03 16:04:46 -0800

    varname fix

--- manifests/groups.pp
+++ manifests/groups.pp
@@ -18,18 +18,18 @@ define pocketprotector::groups::parse (
       # https://www.puppet.com/docs/puppet/8/types/group.html
       group {
         $groupname:
-          allowdupe            => lookup("${usersyaml}.${username}.allowdupe", undef, 'first', undef),
-          attribute_membership => lookup("${usersyaml}.${username}.attribute_membership", undef, 'first', undef),
-          attributes           => lookup("${usersyaml}.${username}.attributes", undef, 'first', undef),
-          auth_membership      => lookup("${usersyaml}.${username}.auth_membership", undef, 'first', undef),
-          ensure               => lookup("${usersyaml}.${username}.ensure", undef, 'first', undef),
-          forcelocal           => lookup("${usersyaml}.${username}.forcelocal", undef, 'first', undef),
-          gid                  => lookup("${usersyaml}.${username}.gid", undef, 'first', undef),
-          ia_load_module       => lookup("${usersyaml}.${username}.ia_load_module", undef, 'first', undef),
-          members              => lookup("${usersyaml}.${username}.members", undef, 'deep', undef),
-          name                 => lookup("${usersyaml}.${username}.name", undef, 'first', undef),
-          provider             => lookup("${usersyaml}.${username}.provider", undef, 'first', undef),
-          system               => lookup("${usersyaml}.${username}.system", undef, 'first', undef),
+          allowdupe            => lookup("${groupyaml}.${groupname}.allowdupe", undef, 'first', undef),
+          attribute_membership => lookup("${groupyaml}.${groupname}.attribute_membership", undef, 'first', undef),
+          attributes           => lookup("${groupyaml}.${groupname}.attributes", undef, 'first', undef),
+          auth_membership      => lookup("${groupyaml}.${groupname}.auth_membership", undef, 'first', undef),
+          ensure               => lookup("${groupyaml}.${groupname}.ensure", undef, 'first', undef),
+          forcelocal           => lookup("${groupyaml}.${groupname}.forcelocal", undef, 'first', undef),
+          gid                  => lookup("${groupyaml}.${groupname}.gid", undef, 'first', undef),
+          ia_load_module       => lookup("${groupyaml}.${groupname}.ia_load_module", undef, 'first', undef),
+          members              => lookup("${groupyaml}.${groupname}.members", undef, 'deep', undef),
+          name                 => lookup("${groupyaml}.${groupname}.name", undef, 'first', undef),
+          provider             => lookup("${groupyaml}.${groupname}.provider", undef, 'first', undef),
+          system               => lookup("${groupyaml}.${groupname}.system", undef, 'first', undef),
       }
     }
   }
