428d4f017103171ff53ce6a16d8f8f10e7e9916d
barry king <barryking93@gmail.com>
2024-04-24 11:23:36 -0700

    adding splunk support

--- manifests/monitoring.pp
+++ manifests/monitoring.pp
@@ -3,11 +3,14 @@
 
 class pocketprotector::monitoring {
   # detect if a server's defined for the varios monitoring server types, install if so
-  if lookup('pocketprotector::monitoring::nagios::server',undef,'deep',false) {
-    include pocketprotector::monitoring::nagios
-  }
   if lookup('pocketprotector::monitoring::librenms::server',undef,'deep',false) {
     include pocketprotector::monitoring::librenms
     include pocketprotector::monitoring::snmp
+  }
+  if lookup('pocketprotector::monitoring::nagios::server',undef,'deep',false) {
+    include pocketprotector::monitoring::nagios
+  }
+  if lookup('pocketprotector::monitoring::splunk::server',undef,'first',false) {
+    include pocketprotector::monitoring::splunk
   }
 }
