eb80c2b2cb6c97b56ea4a5744a1b5a767349e0d4
barry king <barryking93@gmail.com>
2024-11-15 11:28:44 -0800

    don't deploy accounts if they don't exist

--- manifests/accounts.pp
+++ manifests/accounts.pp
@@ -11,25 +11,27 @@
 define pocketprotector::accounts::parse (
   String $usersyaml = $name,
   ){
-  lookup($usersyaml, undef, 'deep', false).each |String $username, Hash $userhash| {
-    #notify {"pocketprotector::accounts: account creation for for ${username}":}
+  if lookup($usersyaml, undef, 'deep', false) {
+    lookup($usersyaml, undef, 'deep', false).each |String $username, Hash $userhash| {
+      #notify {"pocketprotector::accounts: account creation for for ${username}":}
 
-    # https://forge.puppet.com/modules/puppetlabs/accounts/reference
-    accounts::user {
-      $username:
-        comment                  => lookup("${usersyaml}.${username}.comment", undef, 'first', undef),
-        ensure                   => lookup("${usersyaml}.${username}.ensure", undef, 'first', undef),
-        home                     => lookup("${usersyaml}.${username}.home", undef, 'first', "/home/${username}"),
-        gid                      => lookup("${usersyaml}.${username}.gid", undef, 'first', undef),
-        group                    => lookup("${usersyaml}.${username}.group", undef, 'deep', undef),
-        groups                   => lookup("${usersyaml}.${username}.groups", undef, 'deep', undef),
-        ignore_password_if_empty => lookup("${usersyaml}.${username}.ignore_password_if_empty", undef, 'deep', undef),
-        locked                   => lookup("${usersyaml}.${username}.locked", undef, 'first', undef),
-        name                     => lookup("${usersyaml}.${username}.name", undef, 'first', undef),
-        password                 => lookup("pocketprotector::passwords.${username}", undef, 'first', '!!'),
-        shell                    => lookup("${usersyaml}.${username}.shell", undef, 'first', '/bin/bash'),
-        sshkeys                  => lookup("${usersyaml}.${username}.sshkeys", Array[String], 'deep', []),
-        uid                      => lookup("${usersyaml}.${username}.uid", undef, 'first', undef),
+      # https://forge.puppet.com/modules/puppetlabs/accounts/reference
+      accounts::user {
+        $username:
+          comment                  => lookup("${usersyaml}.${username}.comment", undef, 'first', undef),
+          ensure                   => lookup("${usersyaml}.${username}.ensure", undef, 'first', undef),
+          home                     => lookup("${usersyaml}.${username}.home", undef, 'first', "/home/${username}"),
+          gid                      => lookup("${usersyaml}.${username}.gid", undef, 'first', undef),
+          group                    => lookup("${usersyaml}.${username}.group", undef, 'deep', undef),
+          groups                   => lookup("${usersyaml}.${username}.groups", undef, 'deep', undef),
+          ignore_password_if_empty => lookup("${usersyaml}.${username}.ignore_password_if_empty", undef, 'deep', undef),
+          locked                   => lookup("${usersyaml}.${username}.locked", undef, 'first', undef),
+          name                     => lookup("${usersyaml}.${username}.name", undef, 'first', undef),
+          password                 => lookup("pocketprotector::passwords.${username}", undef, 'first', '!!'),
+          shell                    => lookup("${usersyaml}.${username}.shell", undef, 'first', '/bin/bash'),
+          sshkeys                  => lookup("${usersyaml}.${username}.sshkeys", Array[String], 'deep', []),
+          uid                      => lookup("${usersyaml}.${username}.uid", undef, 'first', undef),
+      }
     }
   }
 }
